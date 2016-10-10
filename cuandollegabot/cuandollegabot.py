45c8caadeb4caec7e5e250476703c1279ada05ca
Matias Barriento <elmatibarriento@gmail.com>
2016-10-10 10:50:26 -0300

    Testing update filters

--- cuandollegabot/cuandollegabot.py
+++ cuandollegabot/cuandollegabot.py
@@ -64,7 +64,7 @@ def is_processing(f):
 
 
 @app.route("/bot", methods=['POST'])
-@is_processing
+# @is_processing
 def new_message():
     # logger.debug(os.environ.get("CL_TOKEN", "cuandollegabot"))
     if request.method == "POST":
